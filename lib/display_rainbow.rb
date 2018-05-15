9878c6c24a54da4025d56fbd3b603cab1efab70d
Micah Shute <micah.shute@gmail.com>
2018-05-15 03:11:57 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,6 @@
-# Write your #display_rainbow method here
+# Write your #display_rainbow method
+def display_rainbow(colors)
+  colors.length.times do |index|
+    print index == colors.length - 1 ? "#{colors[index][0].upcase}: #{colors[index]}" + "\n" : "#{colors[index][0].upcase}: #{colors[index]}, "
+  end
+end
